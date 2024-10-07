e534374806a83e48fd645e177ab2f44a2aefbb2f
CANADAGAGOPA <kswms403@naver.com>
2024-10-07 15:12:09 +0900

    init: 프로젝트 초기 생성

---
+++ src/test/java/com/example/TestSpringJWT/TestSpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestSpringJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
