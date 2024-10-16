624b7af5d414aa0044cc1c83b679f5ea96976755
CANADAGAGOPA <kswms403@naver.com>
2024-10-16 12:02:06 +0900

    feat: CORS 설정 추가

--- src/main/java/com/example/TestSpringJWT/config/SecurityConfig.java
+++ src/main/java/com/example/TestSpringJWT/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.TestSpringJWT.config;
 import com.example.TestSpringJWT.config.jwt.JWTFilter;
 import com.example.TestSpringJWT.config.jwt.JWTUtil;
 import com.example.TestSpringJWT.config.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity // 해당 클래스가 스프링 시큐리티에 의해 관리되는 설정 클래스임을 명시
@@ -87,6 +92,29 @@ public class SecurityConfig {
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        // CORS 설정
+        http
+                .cors((cors) -> cors
+                        .configurationSource(new CorsConfigurationSource() {
+
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                                CorsConfiguration configuration = new CorsConfiguration();
+
+                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // 허용할 Origin
+                                configuration.setAllowedMethods(Collections.singletonList("*")); // 허용할 HTTP Method
+                                configuration.setAllowCredentials(true); // Credential 허용 여부
+                                configuration.setAllowedHeaders(Collections.singletonList("*")); // 허용할 Header
+                                configuration.setMaxAge(3600L); // preflight 요청 결과를 캐싱할 시간
+
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization")); // 노출할 Header
+
+                                return null;
+                            }
+                        })
                 );
 
         return http.build();
--- src/main/java/com/example/TestSpringJWT/presentation/MainController.java
+++ src/main/java/com/example/TestSpringJWT/presentation/MainController.java
@@ -1,8 +1,14 @@
 package com.example.TestSpringJWT.presentation;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody // 웹이 아닌 특정 문자열 데이터를 반환
@@ -11,6 +17,15 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "Main Controller";
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority authority = iter.next();
+        String role = authority.getAuthority();
+
+        return "Main Controller " + username + " : " + role;
     }
 }
